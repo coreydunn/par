c70fd14eda27820a20a66556b06b0c0ebee10dc0
coreydunn <jumpingkanga@outlook.com>
2022-11-30 12:21:24 -0600

    multiple input files

--- main.c
+++ main.c
@@ -7,64 +7,74 @@
 
 int main(int argc,char**argv)
 {
-	state=state_new();
+	//state=state_new();
+	Vec args=vec_new(sizeof(char*));
 
 	// Ensure cleanup is called
 	signal(SIGINT,sighandle);
 
 	// Determine string to lex/parse
-	if(argc>1)
+	for(int i=1;i<argc;++i)
+		vec_push(&args,argv+i);
+
+	// Each file in vector
+	for(size_t i=0;i<args.size;++i)
 	{
-		state.infile=fopen(argv[1],"r");
+		state=state_new();
+
+		state.infile=fopen(((const char**)args.buffer)[i],"r");
 		if(!state.infile)
 			err_log("failed to open infile '%s'",argv[1]);
-	}
 
-	// Read input file into buffer
-	if(state.infile)
-	{
-		char file_buffer[1024]={0};
-		size_t count=0;
+		// Read input file into buffer
+		if(state.infile)
+		{
+			char file_buffer[1024]={0};
+			size_t count=0;
 
-		count=fread(file_buffer,1,1000,state.infile);
-		file_buffer[count]=0;
-		str_append(&state.input_buffer,file_buffer);
+			count=fread(file_buffer,1,1000,state.infile);
+			file_buffer[count]=0;
+			str_append(&state.input_buffer,file_buffer);
 
-		fclose(state.infile);
-	}
+			fclose(state.infile);
+		}
 
-	// Lex --> Parse --> Print
-	if(state.input_buffer.buffer)
-	{
-		lex_string(&state.lexer,state.input_buffer.buffer);
-		str_free(&state.input_buffer);
-		//lex_print(&state.lexer);
-		parser_parse(&state.parser,&state.lexer.tokens);
-		lex_free(&state.lexer);
-		pnode_print(&state.parser.root,0);
-		//pnode_print_brief(&state.parser.root,0);
-	}
+		// Lex --> Parse --> Print
+		if(state.input_buffer.buffer)
+		{
+			lex_string(&state.lexer,state.input_buffer.buffer);
+			str_free(&state.input_buffer);
+			//lex_print(&state.lexer);
+			parser_parse(&state.parser,&state.lexer.tokens);
+			lex_free(&state.lexer);
+			//pnode_print(&state.parser.root,0);
+			//pnode_print_brief(&state.parser.root,0);
+		}
 
-	{
-		Str fn=str_new();
-		if(state.infile!=stdin)
-			fn=str_basename(argv[1]);
-		else
-			str_assign(&fn,"a.asm");
-		str_append(&fn,".asm");
+		{
+			Str fn=str_new();
+			if(state.infile!=stdin)
+				fn=str_basename(((char**)args.buffer)[i]);
+			else
+				str_assign(&fn,"a.asm");
+			str_append(&fn,".asm");
 
-		state.outfile=fopen(fn.buffer,"w");
-		if(!state.outfile)state.outfile=stdout;
-		if(!state.outfile){err_log("cannot open outfile '%s'","a.out");state.outfile=stdout;}
+			state.outfile=fopen(fn.buffer,"w");
+			if(!state.outfile)state.outfile=stdout;
+			if(!state.outfile){err_log("cannot open outfile '%s'","a.out");state.outfile=stdout;}
 
-		gen_x86_64(&state.parser.root,state.outfile);
+			gen_x86_64(&state.parser.root,state.outfile);
 
-		if(state.outfile)
-			fclose(state.outfile);
-		str_free(&fn);
+			if(state.outfile)
+				fclose(state.outfile);
+			str_free(&fn);
+		}
+
+		state_free(&state);
 	}
 
 	// Free memory and leave
 	state_free(&state);
+	vec_free(&args);
 	cleanquit(0);
 }
--- state.c
+++ state.c
@@ -6,15 +6,15 @@ State state;
 
 void cleanquit(int exitval)
 {
-		puts("bye");
-			state_free(&state);
-				exit(exitval);
+	puts("bye");
+	state_free(&state);
+	exit(exitval);
 }
 
 void sighandle(int sig)
 {
-		if(sig==SIGINT)
-					cleanquit(0);
+	if(sig==SIGINT)
+		cleanquit(0);
 }
 
 State state_new(void)
@@ -33,7 +33,7 @@ void state_free(State*st)
 {
 	if(!st)
 	{
-		fprintf(stderr,"error: NULL State pointer passed to state_free\n");
+		err_log("NULL State pointer passed to state_free");
 		return;
 	}
 
