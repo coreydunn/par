1c9ca72bca6e99e8549edb1c01b97bae75ad6475
coreydunn <jumpingkanga@outlook.com>
2022-12-22 16:34:54 -0600

    proper test return value

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -12,15 +12,15 @@ assert()
 	local c_green='\033[0;32m'
 	local c_yellow='\033[0;33m'
 	local c_normal='\033[0;0m'
-	local succeed=0
 
 	printf "$c_yellow""$1""$c_normal"" => "
 	if [ -e $1 ]; then
 		$("./$1")
-		if [ $? = 0 ]; then
+		local result="$?"
+		if [ "$result" = 0 ]; then
 			printf "$c_green""OK\n""$c_normal"
 		else
-			printf "$c_red""FAIL""$c_normal"" ($?)\n"
+			printf "$c_red""FAIL""$c_normal"" ($result)\n"
 		fi
 	else
 		printf "$c_red""test not built\n""$c_normal"
